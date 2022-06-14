6d9eb2cf94c848a653f45f3389afc4aa9605f76b
chaudhuree <chaudhuree00@gmail.com>
2022-06-15 05:58:46 +0600

    docs: initial font end setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 
+const sendMessage = () => {
+  console.log("sendMessage");
+}
 function App() {
   return (
     <div className="App">
-      
+    <input type="text" placeholder="message.." />
+      <button onClick={sendMessage}>Send Message</button>
     </div>
   );
 }
