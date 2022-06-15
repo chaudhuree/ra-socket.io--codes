532c86b792aefa42b325d62785552e7408343244
chaudhuree <chaudhuree00@gmail.com>
2022-06-15 06:42:58 +0600

    docs: data transmission between client and server

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
+import { useEffect } from 'react';
+import io from "socket.io-client";
 import './App.css';
 
+const socket = io.connect("http://localhost:3001");
+
+
 const sendMessage = () => {
-  console.log("sendMessage");
+  socket.emit("send_message", { message:"hello" });
 }
 function App() {
+  useEffect(() =>{
+    socket.on('receive_message',data=>{
+      alert(data.message);
+    })
+  },[socket])
   return (
     <div className="App">
     <input type="text" placeholder="message.." />
--- server/server.js
+++ server/server.js
@@ -18,6 +18,13 @@ const io = new Server(server, {
   },
 });
 
+io.on('connection',socket=>{
+  console.log('a user connected with id: ', socket.id);
+  socket.on('send_message',(data)=>{
+    console.log(data);
+    socket.broadcast.emit('receive_message',data);
+  })
+})
 server.listen(3001, () => {
   console.log("SERVER IS RUNNING");
 });
